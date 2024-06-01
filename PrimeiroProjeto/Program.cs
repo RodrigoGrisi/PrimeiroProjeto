914d9c77eeac291ac4f56b3dead846e77c7bf499
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2024-05-31 21:04:50 -0300

    BUILT-IN DATA TYPES - TIPOS DE VALOR c#

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,4 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace PrimeiroProjeto {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("First backend desenvolviment");
 
+            sbyte x = 100;
+            Console.WriteLine(x);
+
+            //RETORNA O VALOR 0 - POR CONTA DO OVERFLOW
+            byte n1 = 255;
+            n1++;
+            Console.WriteLine(n1);  
+            
+            int n2 = -126;
+            Console.WriteLine(n2);
+            Console.WriteLine(  );
+        }
+    }
+}
+
+//IDENTAÇÃO AUTOMATICA CTRL+K+D
+//EXECUTAR CODIGO CTRL+F5
+// DUPLICAR LINE CTRL+D
