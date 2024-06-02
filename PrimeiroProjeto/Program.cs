c393abb14fa6bab1f0a373aed58cac24f437349e
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2024-06-01 22:03:37 -0300

    OperadoresComparativos - ENTRADA DE DADOS

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,25 +1,73 @@
 ﻿using System;
+using System.Globalization;
 
 namespace PrimeiroProjeto {
     internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("First backend desenvolviment");
+        static void EntradaDeDados(string[] args) {
+            Console.Write("Digite o seu nome: ");
+            string nome = Console.ReadLine();
 
-            sbyte x = 100;
-            Console.WriteLine(x);
+            Console.Write("Digite a sua Idade: ");
+            int idade = int.Parse(Console.ReadLine());
 
-            //RETORNA O VALOR 0 - POR CONTA DO OVERFLOW
-            byte n1 = 255;
-            n1++;
-            Console.WriteLine(n1);  
-            
-            int n2 = -126;
-            Console.WriteLine(n2);
-            Console.WriteLine(  );
+            Console.Write("Digite o preço do seu produto: ");
+            string preco = Console.ReadLine();
+
+            Console.Write("Digite a sua Altura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"{nome} , tem {idade} , o preço do seu produto " +
+                $" é de R$ {preco} e sua altura é {altura.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture)}m.");
+
         }
+
+        static void EntradaSoma(string[] args) {
+
+            int value1, value2, soma;
+
+            Console.Write("Digite o primeiro valor: ");
+            value1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            value2 = int.Parse(Console.ReadLine());
+
+            soma = value1 + value2;
+
+            Console.WriteLine(soma);
+
+        }
+
+        static void EstrturaSequencial(string[] args) {
+
+            Console.WriteLine("Digite o valor do raio do circulo: ");
+            double value = double.Parse(Console.ReadLine());
+            double TT = 3.14159;// subistiuir pelo PI
+
+            //FORMULA DA AREA 
+            double result = Math.PI * Math.Pow(value, 2);
+
+            Console.WriteLine($"O valor da area deste circulo é de {result.ToString("F4", CultureInfo.InvariantCulture)}");
+
+        } 
+        static void OperadoresComparativos(string[] args) {
+            
+
+            int value1 = 20;
+            int value2 = 10; 
+            
+            int value3 = 25;
+            double value4 = 25;
+
+            bool c1 = (value1 < value2);
+            bool c2 = (value3 != value4);
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+
+
+        }
+
+        static void Main(string[] args) { }
     }
 }
-
-//IDENTAÇÃO AUTOMATICA CTRL+K+D
-//EXECUTAR CODIGO CTRL+F5
-// DUPLICAR LINE CTRL+D
